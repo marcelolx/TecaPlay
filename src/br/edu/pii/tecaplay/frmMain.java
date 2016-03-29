1e65a0698913029af047dfe9dc9b9460113d48d1
Jonas Halmenschlager <jonassjo@outlook.com>
2016-03-29 19:57:18 -0300

    Importando o projeto para o BitBucket.

---
+++ src/br/edu/pii/tecaplay/frmMain.java
@@ -0,0 +1,40 @@
+package br.edu.pii.tecaplay;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
+import javax.swing.JTabbedPane;
+import javax.swing.JToolBar;
+
+public class frmMain extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmMain frame = new frmMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public frmMain() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+	}
+
+}

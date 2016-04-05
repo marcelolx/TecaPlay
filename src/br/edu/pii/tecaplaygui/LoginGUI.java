3b5e02e8085ff066eb5208b0efc0cce22e43ac2d
Marcelo <Marcelo@172.19.17.136>
2016-04-04 21:20:22 -0300

    Interface do login alterada.

--- src/br/edu/pii/tecaplaygui/LoginGUI.java
+++ src/br/edu/pii/tecaplaygui/LoginGUI.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,7 +17,8 @@ public class LoginGUI {
 		 */
 		JFrame login = new JFrame();
 		login.setVisible(true);
-		login.setSize(250, 70);
+		login.setSize(320, 250);
+		login.setResizable(false);
 		login.setTitle("Login - TecaPlay");
 		login.setLocationRelativeTo(null);
 		login.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\Marcelo\\git\\tecaplay\\images\\newLogo.png"));
@@ -30,8 +32,11 @@ public class LoginGUI {
 		login.getContentPane().add(logPanel, BorderLayout.CENTER);
 		JLabel userField = new JLabel("Usuário: ");
 		JTextField usr = new JTextField();
+		ImageIcon imag = new ImageIcon("C:\\Users\\Marcelo\\git\\tecaplay\\images\\newLogo4.png");
+		JLabel img = new JLabel(imag);
 		usr.setBackground(new Color(192, 192, 192));
-		usr.setColumns(15);
+		usr.setColumns(21);
+		logPanel.add(img, BorderLayout.NORTH);
 		logPanel.add(userField, BorderLayout.CENTER);
 		logPanel.add(usr, BorderLayout.CENTER);
 	}

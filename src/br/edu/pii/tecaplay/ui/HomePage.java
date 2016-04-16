b956bf218280f5bd87ed1ee110c50b04745bb93a
Marcelo <marcelolauxen16@gmail.com>
2016-04-15 21:43:33 -0300

    Botão voltar, na home.

--- src/br/edu/pii/tecaplay/ui/HomePage.java
+++ src/br/edu/pii/tecaplay/ui/HomePage.java
@@ -136,9 +136,22 @@ public class HomePage {
 		panelLateralVideo.setPreferredSize(new Dimension(180,400));
 		panelLateralVideo.setBackground(new Color(128,128,128));
 		
-		JButton btnVoltar = new JButton();
+		JButton btnVoltar = new JButton("<--");
 		btnVoltar.setToolTipText("Voltar");
-		
+		btnVoltar.setPreferredSize(new Dimension(86, 34));
+		panelLateralVideo.add(btnVoltar);
+		btnVoltar.setBackground(Color.LIGHT_GRAY);
+		btnVoltar.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				home.getContentPane().add(panelTopo, BorderLayout.NORTH);
+				panelLateralVideo.setVisible(false);
+				home.getContentPane().remove(panelLateralVideo);
+				panelTopo.setVisible(true);
+				panelTopo.revalidate();
+			}
+		});	
 		
 		//jButton Adicionar
 		JButton btnAdicionar = new JButton("Adicionar");
